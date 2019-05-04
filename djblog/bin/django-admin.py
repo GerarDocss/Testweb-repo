cd47f61ac77ad6f846437bd5577c728e68d6d9ea
Gerardo Collado <ge.dardo@gmail.com>
2019-05-03 21:16:21 -0500

    Fist commit

---
+++ djblog/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/samsa/Desktop/13-Deployment-Heroku/django_project/djblog/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
