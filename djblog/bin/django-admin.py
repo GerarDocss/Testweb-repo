5580c841d3815e3f1c0f89bc68daded7b26765b1
Gerardo Collado <ge.dardo@gmail.com>
2019-05-15 15:58:08 -0500

    Rama con la adición de about

--- djblog/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/samsa/Desktop/13-Deployment-Heroku/django_project/djblog/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
