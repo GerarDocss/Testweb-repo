38df249ea083256c61244e709cd9b87ee2f14228
Gerardo Collado <ge.dardo@gmail.com>
2019-05-24 10:52:15 -0500

    se añadio gráfica highcharts

--- sensor/views.py
+++ sensor/views.py
@@ -1,8 +1,27 @@
+from django.db.models import Count, Q
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
 
 def temperature(request):
 
-    return render(request, 'sensor/about.html', {'title': 'Sensor'})
+    dataset = {
+        'posts': posts
+}
+
+    return render(request, 'sensor/about.html', {'dataset': dataset})
 
